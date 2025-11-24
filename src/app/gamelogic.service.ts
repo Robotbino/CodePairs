36bb1d843d5113f446f11f9cce2149621a4f87c7
Robotbino <hlongwanabino@gmail.com>
2025-11-24 02:56:21 +0200

    Character neglected when cleaning code

--- src/app/gamelogic.service.ts
+++ src/app/gamelogic.service.ts
@@ -345,7 +345,7 @@ export class GamelogicService {
   }
 
   private shuffleCards(): void {
-    /
+    
     for (let i = this.cards.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
